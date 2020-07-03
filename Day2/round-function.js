3397f744d2da13aceb748dc79677c7578da9dc43
Kang <kang.12@live.com>
2020-07-03 13:37:02 +0700

    commit Day2

--- Day2/round-function.js
+++ Day2/round-function.js
@@ -67,3 +67,5 @@ console.log('random 0 to 255', numrand3)
 let java = "JavaScript"
 let randindex= Math.floor(Math.random()* java.length+1)
 console.log(java[randindex])
+
+console.log(' 1 1 1 1 1 \n 2 1 2 4 8 \n 3 1 3 9 27 \n 4 1 4 16 64 \n 5 1 5 25 125 ')
